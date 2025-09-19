d24ff9b571e018c55a94308be3f80f44c9566f75
Hüseyin Mert <huseyinin@gmail.com>
2025-09-19 17:12:08 +0300

    Refactor configuration loading to use YAML files and set defaults in init function

--- config/config.go
+++ config/config.go
@@ -1,7 +1,10 @@
 package config
 
 import (
-	"github.com/jinzhu/configor"
+	"fmt"
+	"os"
+
+	"github.com/goccy/go-yaml"
 )
 
 type DBConfig struct {
@@ -30,7 +33,7 @@ type AppConfig struct {
 
 	// Auth Max Refresh - minutes
 	MaxRefresh uint `default:"60"`
-	
+
 	// Auth Key
 	Key string `default:"12345678" env:"AUTH_KEY"`
 
@@ -45,7 +48,48 @@ var Config = struct {
 }{}
 
 func init() {
-	if err := configor.Load(&Config, "config/database.yml", "config/smtp.yml", "config/application.yml"); err != nil {
-		panic(err)
+	// Set sane defaults (previously provided by struct tags via configor)
+	Config.Db = DBConfig{
+		Name:     "ibanim",
+		Adapter:  "postgres",
+		Host:     "localhost",
+		Port:     "5432",
+		User:     "ibanim",
+		Password: "ibanim",
 	}
+	Config.App = AppConfig{
+		Port:       7000,
+		Env:        "localhost",
+		Debug:      false,
+		Timeout:    60,
+		MaxRefresh: 60,
+		Key:        "12345678",
+		Realm:      "ibanim zone",
+	}
+
+	// Load YAML files if present; missing files are ignored
+	for _, path := range []string{
+		"config/database.yml",
+		"config/smtp.yml",
+		"config/application.yml",
+	} {
+		if err := loadYAML(path); err != nil {
+			panic(err)
+		}
+	}
+}
+
+// loadYAML unmarshals the YAML file into the global Config, updating only fields present in the file.
+func loadYAML(path string) error {
+	b, err := os.ReadFile(path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return nil
+		}
+		return fmt.Errorf("read %s: %w", path, err)
+	}
+	if err := yaml.Unmarshal(b, &Config); err != nil {
+		return fmt.Errorf("parse %s: %w", path, err)
+	}
+	return nil
 }
--- go.mod
+++ go.mod
@@ -22,6 +22,7 @@ require (
 	github.com/bytedance/sonic v1.14.1 // indirect
 	github.com/bytedance/sonic/loader v0.3.0 // indirect
 	github.com/cloudwego/base64x v0.1.6 // indirect
+	github.com/fatih/color v1.10.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.10 // indirect
 	github.com/gin-contrib/sse v1.1.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
@@ -29,6 +30,7 @@ require (
 	github.com/go-playground/validator/v10 v10.27.0 // indirect
 	github.com/go-sql-driver/mysql v1.9.3 // indirect
 	github.com/goccy/go-json v0.10.5 // indirect
+	github.com/goccy/go-yaml v1.12.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgx/v5 v5.5.5 // indirect
@@ -39,6 +41,7 @@ require (
 	github.com/klauspost/cpuid/v2 v2.3.0 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
+	github.com/mattn/go-colorable v0.1.8 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
@@ -51,6 +54,7 @@ require (
 	golang.org/x/sync v0.17.0 // indirect
 	golang.org/x/sys v0.36.0 // indirect
 	golang.org/x/text v0.29.0 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	google.golang.org/protobuf v1.36.9 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
